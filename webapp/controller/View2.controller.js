323e1e4ed9f624769636685b998e1a658c0a6541
Shobhit Tyagi <shobhittyagimit@gmail.com>
2024-03-13 09:53:11 +0000

    commit 2nd

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -44,10 +44,70 @@ sap.ui.define(
 
                     }
                 ];
+                let sDropdown = [
+                    {
+                        "filename": "State registration certificate of legal entity.xls",
+                        "mediaType": "sap-icon://excel-attachment",
+    
+                        "type": "State registration certificate of legal entity"
+    
+                    },
+                    {
+                        "filename": "Certificate of VAT registration.pdf",
+                        "mediaType": "sap-icon://pdf-attachment",
+    
+                        "type": "Certificate of VAT registration.png"
+                    },
+                    {
+                        "filename": "Tax registration certificate.txt",
+                        "mediaType": "sap-icon://doc-attachment",
+    
+                        "type": "Tax registration certificate"
+                    }];
+                    let gmodel = new JSONModel(sDropdown);
+                    this.getView().setModel(gmodel, "sModel");
 
                 let qmodel = new JSONModel(qDropdown);
                 this.getView().setModel(qmodel, "qsModel");
-            }
+            },
+            itemValidationCallback: function (oItemInfo) {
+                const { oItem, iTotalItemsForUpload } = oItemInfo;
+                var oUploadSetTableInstance = this.byId("UploadSetTable1");
+                var oSelectedItems = oUploadSetTableInstance.getSelectedItems();
+                var oSelectedItemForUpdate = oSelectedItems.length === 1 ? oSelectedItems[0] : null;
+                if (oSelectedItemForUpdate && oSelectedItemForUpdate.getFileName() === "-" && iTotalItemsForUpload === 1) {
+                    return new Promise((resolve) => {
+                        if (oSelectedItemForUpdate) {
+                            var oContext = oSelectedItemForUpdate.getBindingContext();
+                            var data = oContext && oContext.getObject ? oContext.getObject() : {};
+
+                            /* Demonstration use case of Setting the header field if required to be passed in API request headers to
+                                    inform backend with document type captured through user input */
+                            oItem.addHeaderField(new CoreItem(
+                                {
+                                    key: "existingDocumentID",
+                                    text: data ? data.id : ""
+                                }
+                            ));
+                        }
+                        resolve(oItem);
+                    });
+                } else {
+                    var oItemPromise = new Promise((resolve, reject) => {
+                        this.oItemsProcessor.push({
+                            item: oItem,
+                            resolve: resolve,
+                            reject: reject
+                        });
+                    });
+                    if (iTotalItemsForUpload === 1) {
+                        this.openFileUploadDialog();
+                    } else if (iTotalItemsForUpload === this.oItemsProcessor.length) {
+                        this.openFileUploadDialog();
+                    }
+                    return oItemPromise;
+                }
+            },
         });
     }
 );
